024c94f3a23b107a7436edcea7dfe704bebda555
Vishal Saranathan <s-vishal1999@hotmail.com>
2020-06-07 02:02:42 +0530

    feat: evaluator done

--- evaluator.py
+++ evaluator.py
@@ -1,41 +1,80 @@
 import cv2
+import operator
 import numpy as np
-
-image = cv2.imread('./test-image.jpg', 0)
-
-scale = 40
-width = int(image.shape[1] * scale / 100)
-height = int(image.shape[0] * scale / 100)
-dim = (width, height)
-image = cv2.resize(
-    image, dim, interpolation=cv2.INTER_AREA)
-thresh = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                               cv2.THRESH_BINARY, 199, 32)
-
-cv2.imshow('image', thresh)
-cv2.waitKey(0)
-blur = cv2.GaussianBlur(thresh, (5, 5), cv2.BORDER_DEFAULT)
-kernel = np.ones((5, 5), np.uint8)
-blur = cv2.dilate(blur, kernel, iterations=1)
-
-thresh = cv2.bitwise_not(blur)
-ctrs, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
-                              cv2.CHAIN_APPROX_SIMPLE)
-sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+from test import character_predict
 
 
-# TODO: morphx_ex
+def image_preprocess(image):
+    thresh = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv2.THRESH_BINARY, 199, 32)
 
-for i, ctr in enumerate(sorted_ctrs):
-    # Get bounding box
-    x, y, w, h = cv2.boundingRect(ctr)
-    # Getting ROI
-    roi = image[y-15:y+h+15, x-15:x+w+15]
+    blur = cv2.GaussianBlur(thresh, (5, 5), cv2.BORDER_DEFAULT)
+    kernel = np.ones((5, 5), np.uint8)
+    blur = cv2.dilate(blur, kernel, iterations=1)
 
-    # show ROI
-    #cv2.imwrite('roi_imgs.png', roi)
-    cv2.imshow('charachter'+str(i), roi)
-    # cv2.rectangle(image, (x-50, y-50),
-    #               (x + w + 50, y + h + 50), (90, 0, 255), 2)
-    # cv2.imshow("rec", image)
-    cv2.waitKey(0)
+    thresh = cv2.bitwise_not(blur)
+    return thresh
+
+
+def seg_predict(image, cnt):
+    # TODO: morphx_ex
+    character_image = []
+
+    for i, ctr in enumerate(cnt):
+
+        x, y, w, h = cv2.boundingRect(ctr)
+        roi = image[y-15:y+h+15, x-15:x+w+15]
+        roi_f = cv2.resize(
+            roi, (128, 128), interpolation=cv2.INTER_AREA)
+
+        roi_f = cv2.cvtColor(roi_f, cv2.COLOR_BGR2RGB)
+        character_image.append(roi_f)
+        # cv2.imshow('character'+str(i), roi_f)
+
+        cv2.waitKey(0)
+
+    return list(map(character_predict, character_image))
+
+
+def calculate(character):
+    op1 = 0
+    op2 = 0
+    flag = False
+
+    for i in range(len(character)):
+        if character[i].isdigit() and not flag:
+            op1 = op1*10 + int(character[i])
+
+        if character[i].isdigit() == False:
+            flag = True
+
+        if character[i].isdigit() and flag:
+            op2 = op2*10 + int(character[i])
+
+    ops = {"+": operator.add, "-": operator.sub,
+           "*": operator.mul, "/": operator.truediv}
+    return ops[character[1]](op1, op2)
+
+
+def main(image_path):
+    image = cv2.imread(image_path, 0)
+    # scale = 40
+    # width = int(image.shape[1] * scale / 100)
+    # height = int(image.shape[0] * scale / 100)
+    # dim = (width, height)
+    # image = cv2.resize(
+    #     image, dim, interpolation=cv2.INTER_AREA)
+    prepped = image_preprocess(image)
+    ctrs, hier = cv2.findContours(prepped, cv2.RETR_EXTERNAL,
+                                  cv2.CHAIN_APPROX_SIMPLE)
+    sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+    characters = seg_predict(image, sorted_ctrs)
+    output = calculate(characters)
+    print(output)
+
+
+if __name__ == "__main__":
+    model_path = "model/model-bst.h5"
+    image_path = "test-image.jpg"
+    main(image_path)
--- test.py
+++ test.py
@@ -8,21 +8,20 @@ import tensorflow.keras as ks
 def character_predict(image, model_path="model/model-bst.h5"):
 
     model = ks.models.load_model(model_path)
-    test_datagen = ImageDataGenerator(
-        rescale=1./255
-    )
-    # image = cv2.imread("./data/test/nine/481.jpg")
+
+    # image = cv2.imread(image)
     image = image.astype('float') / 255.0
 
     image = cv2.resize(
         image, (128, 128))
-
     image = ks.preprocessing.image.img_to_array(image)
     image = np.expand_dims(image, axis=0)
 
     pred = model.predict(image)
-    ic = pred.argmax(axis=1)[0]
-
+    print(pred)
+    # TODO: Retrain model in Colab and uncomment below
+    # ic = pred.argmax(axis=1)[0]
+    ic = np.argsort(-pred, axis=1)[0][1]
     image_class = [".", "/", "8", "=", "5", "4", "-",
                    "9", "1", "+", "7", "6", "3", "*", "2", "0"]
 
